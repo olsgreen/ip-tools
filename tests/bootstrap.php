b29ff86ec683cd4a25aa767abe20cd04750ea5e9
Joe Huss <detain@interserver.net>
2018-05-17 22:16:12 -0400

    updates

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -25,7 +25,7 @@ $root = realpath(dirname(dirname(__FILE__)));
  * Check that --dev composer installation was done
  */
 if (!file_exists($root . '/vendor/autoload.php')) {
-	throw new \Exception('Please run "php composer.phar install --dev" in root directory to setup unit test dependencies before running the tests');
+    throw new \Exception('Please run "php composer.phar install --dev" in root directory to setup unit test dependencies before running the tests');
 }
 
 // Include the Composer autoloader
