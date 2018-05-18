3169d641489050b35ebe39de2c993e17923bb306
Joe Huss <detain@interserver.net>
2018-05-17 21:17:06 -0400

    work on tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,17 +19,13 @@ error_reporting(-1);
  */
 date_default_timezone_set('UTC');
 
-
 $root = realpath(dirname(dirname(__FILE__)));
 
 /**
  * Check that --dev composer installation was done
  */
 if (!file_exists($root . '/vendor/autoload.php')) {
-    throw new \Exception(
-        'Please run "php composer.phar install --dev" in root directory '
-        . 'to setup unit test dependencies before running the tests'
-    );
+	throw new \Exception('Please run "php composer.phar install --dev" in root directory to setup unit test dependencies before running the tests');
 }
 
 // Include the Composer autoloader
